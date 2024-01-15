ffee439ae11d7d9a202568ae866c36d389c82798
Eero Vahteri <eero.vahteri@helsinki.fi>
2024-01-15 12:13:31 +0200

    5.3 done

--- Osa5/bloglist-frontend/src/App.jsx
+++ Osa5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -75,13 +78,61 @@ const App = () => {
     )
   }
 
+  const handleNewblog = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const blog = await blogService.create({
+        title, author, url
+      })
+    } catch (exception) {
+      setErrorMessage('something went wrong')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
-      
+
       <button onClick={() => window.localStorage.removeItem('loggedBlogappUser')}>logout</button>
       
+      <h2>create new</h2>
+
+      <form onSubmit={handleNewblog}>
+        <div>
+          title:
+            <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+            <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+            <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
