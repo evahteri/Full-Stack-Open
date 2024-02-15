3e5a2552f0c785da557a57c68fe0598169ceedc1
Eero Vahteri <eero.vahteri@helsinki.fi>
2024-02-15 15:19:10 +0200

    5.10 done

--- Osa5/bloglist-frontend/src/App.jsx
+++ Osa5/bloglist-frontend/src/App.jsx
@@ -126,9 +126,10 @@ const App = () => {
       
       {blogForm()}
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => <Blog key={blog.id} blog={blog} />)
+      }
     </div>
   )
 }
