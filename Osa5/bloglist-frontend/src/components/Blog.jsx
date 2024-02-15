f50923ec95bd0465256c80af532758e11faa0461
Eero Vahteri <eero.vahteri@helsinki.fi>
2024-02-15 13:54:31 +0200

    5.7 done

--- Osa5/bloglist-frontend/src/components/Blog.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullInfo, setfullInfo] = useState(false)
+
+
+  const toggleFullInfo = () => {
+    if (fullInfo)
+      setfullInfo(false)
+    else
+      setfullInfo(true)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  if (fullInfo)
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={toggleFullInfo}>hide</button> <br />
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button> <br />
+        {blog.author}
+      </div>)
+  else
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleFullInfo}>view</button>
+      </div>
+    )
+}
+
+
+
 
 export default Blog
